fe32dc6f6d5f5561b9bc4e29f2d9d0e8a290c7ca
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-12-09 15:54:18 -0800

    empty out default route

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -14,13 +14,7 @@ define([
                 'circle' : 'openCircular'
             },
             openApplication : function() {
-                var appView = new AppView({
-                    //always inject the container element into the view
-                    el : $('body')
-                });
-                    //call the render function immediately.  
-                    //asynchronous actions should be created after the template render
-                appView.render();
+               
             },
             openCircular : function () {
                 var circularView = new CircularView({

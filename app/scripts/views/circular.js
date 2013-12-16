679264f525f3c61c3997272cb769eb4047ee2936
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-12-16 14:43:55 -0800

    add circvis layout

--- app/scripts/views/circular.js
+++ app/scripts/views/circular.js
@@ -3,21 +3,23 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'views/application',
-    'vq',
+    'views/base',
     'helpers/circvis_helper',
+    'vq',
     'circvis'
-], function ($, _, Backbone, AppView, vq, CircvisConfigFactory) {
+], function ($, _, Backbone, BaseView, CircvisConfigFactory, vq, Circvis) {
 
-    var CircularView = AppView.extend({
+    var CircularView = BaseView.extend({
     	//the template file is defined relative to the path /app/scripts/templates
         // see main.js to modify this configuration
-        template: 'application.hbs',
+        template: 'circular.hbs',
         
         //afterRender is executed immediately after the view's document fragment is injected into the DOM.
         //This is the first opportunity to select, modify, or attach handlers to the view's DOM fragment
         afterRender: function() {
-            CircvisConfigFactory.data().rings([{}]);
+            var factory = CircvisConfigFactory.container('.circvis-container').data().rings([{}]);
+            var circvis = new vq.CircVis(factory.config());
+            circvis();
         }
     });
 

626e89825e959efdd8b7b1a80a3830d31575b6bf
Dick Kreisberg <rkreisberg@systemsbiology.org>
2014-07-24 10:11:26 -0700

    clean up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,6 @@
 module React from 'react';
 import {GridView} from './components/GridView.react.js';
 
-
 const render = () => React.renderComponent(
     new GridView(null),
     document.getElementById('content')
